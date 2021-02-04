e49990d2384976e68770846e91d4853b95377264
laradoren <48968796+laradoren@users.noreply.github.com>
2021-02-04 20:59:02 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Header} from "./components/Header";
+import {Footer} from "./components/Footer";
+import {Board} from "./components/Board";
+import {useSelector} from "react-redux";
+import {BoardState} from "./redux/boardReducer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+    const [darkTheme, setDarkTheme] = useState(false);
+    const [openModalAddNewCard, setOpenModalAddNewCard] = React.useState(false);
+    const state = useSelector<BoardState, BoardState>(
+        (state) => state
+    );
+
+    const handleOpenModal = () => {
+        setOpenModalAddNewCard(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenModalAddNewCard(false);
+    };
+    return (
+        <div>
+          <Header darkTheme = {darkTheme} setDarkTheme={setDarkTheme} handleOpenModal={handleOpenModal} />
+          <Board darkTheme = {darkTheme} state = {state} openModal={openModalAddNewCard} handleCloseModal={handleCloseModal} />
+          <Footer />
+        </div>
   );
 }
 
